f205edd5290d49df6d361b547d82b84f87d047ce
soeun1230 <ybbsoen1230@naver.com>
2024-04-30 10:38:00 +0900

    actor multiport로 보내기 1

--- src/main/java/backend/musicalmate/oauth/kakao/KakaoApiClient.java
+++ src/main/java/backend/musicalmate/oauth/kakao/KakaoApiClient.java
@@ -1,13 +1,9 @@
 package backend.musicalmate.oauth.kakao;
 
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
-import org.springframework.web.service.annotation.PostExchange;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED_VALUE;
 
 //HTTP Interface Client 사용
 public interface KakaoApiClient {
